1569d6ec2c178d4fd564f3b8ccca56f5076eda6e
DESKTOP-SRAB6QF\User <rkrishnamohan96@gmail.com>
2023-06-22 15:37:53 +0530

    Assignment completion

--- routes/vision.js
+++ routes/vision.js
@@ -1,16 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const  AWS = require('aws-sdk');
+require('dotenv').config()
+
+// Configure AWS credentials
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+let rekognition = new AWS.Rekognition();
 
 router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
+  // Access the uploaded image file
+  const uploadedFile = req.files.file;
 
-  // Your code starts here //
+  let params = {
+    Image: {
+      Bytes: uploadedFile.data
+    },
+    MaxLabels: 10 // Maximum number of labels to retrieve
+  };
 
-  // Your code ends here //
+  rekognition.detectLabels(params, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Unable to process the request' });
+    } else {
+      // Extract the labels from the API response
+      let labels = data.Labels.map(function(label) {
+        return label.Name;
+      });
 
-  res.json({
-    "labels": response
+      res.json({
+        labels: labels
+      });
+    }
   });
 });
 
